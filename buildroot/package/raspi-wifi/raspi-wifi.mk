5659aa056ffe4746cf2ff49585d0b1bfb647b6a9
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2026-05-26 14:49:41 +0200

    fix: add dnsmasq.service for SoftAP DHCP
    dnsmasq binary was installed but had no systemd service unit.
    snapdog-ctrl calls 'systemctl start dnsmasq' for AP mode.

--- buildroot/package/raspi-wifi/raspi-wifi.mk
+++ buildroot/package/raspi-wifi/raspi-wifi.mk
@@ -11,6 +11,8 @@
 define RASPI_WIFI_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_SNAPDOG_PATH)/package/raspi-wifi/hostapd.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/hostapd.service
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_SNAPDOG_PATH)/package/raspi-wifi/dnsmasq.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/dnsmasq.service
 endef
 
 $(eval $(generic-package))

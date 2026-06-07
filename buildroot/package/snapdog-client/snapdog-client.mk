207d9c7cad7aaf49ced46c572958e344ab65b825
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2026-06-07 02:25:54 +0200

    fix: update snapdog client/server download URL to SnapDogRocks org

--- buildroot/package/snapdog-client/snapdog-client.mk
+++ buildroot/package/snapdog-client/snapdog-client.mk
@@ -6,7 +6,7 @@
 
 SNAPDOG_CLIENT_VERSION = 0.11.3
 SNAPDOG_CLIENT_SOURCE = snapdog-v$(SNAPDOG_CLIENT_VERSION)-aarch64-unknown-linux-gnu.tar.gz
-SNAPDOG_CLIENT_SITE = https://github.com/metaneutrons/snapdog/releases/download/v$(SNAPDOG_CLIENT_VERSION)
+SNAPDOG_CLIENT_SITE = https://github.com/SnapDogRocks/snapdog/releases/download/v$(SNAPDOG_CLIENT_VERSION)
 SNAPDOG_CLIENT_LICENSE = GPL-3.0-only
 
 define SNAPDOG_CLIENT_INSTALL_TARGET_CMDS
--- buildroot/package/snapdog-server/snapdog-server.mk
+++ buildroot/package/snapdog-server/snapdog-server.mk
@@ -8,7 +8,7 @@
 
 SNAPDOG_SERVER_VERSION = $(SNAPDOG_CLIENT_VERSION)
 SNAPDOG_SERVER_SOURCE = snapdog-v$(SNAPDOG_SERVER_VERSION)-aarch64-unknown-linux-gnu.tar.gz
-SNAPDOG_SERVER_SITE = https://github.com/metaneutrons/snapdog/releases/download/v$(SNAPDOG_SERVER_VERSION)
+SNAPDOG_SERVER_SITE = https://github.com/SnapDogRocks/snapdog/releases/download/v$(SNAPDOG_SERVER_VERSION)
 SNAPDOG_SERVER_LICENSE = GPL-3.0-only
 
 define SNAPDOG_SERVER_INSTALL_TARGET_CMDS
